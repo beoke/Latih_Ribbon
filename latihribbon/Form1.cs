6331338a519944b9427d448a48aaa4e18faff717
Dhafa <dhafabintangr20@gmail.com>
2024-08-02 11:12:55 +0700

    Add project files.

---
+++ latihribbon/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace latihribbon
+{
+    public partial class Form1 : RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ribbon1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
