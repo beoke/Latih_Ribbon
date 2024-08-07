5520d1dbe251d9a474c0bdac33b0b254e9a29298
Dhafa <dhafabintangr20@gmail.com>
2024-08-07 12:01:31 +0700

    edit label fungsi

--- latihribbon/FormMilih.Designer.cs
+++ latihribbon/FormMilih.Designer.cs
@@ -32,10 +32,6 @@ namespace latihribbon
         {
             this.btn_Keluar = new System.Windows.Forms.Button();
             this.btn_masuk = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -67,46 +63,6 @@ namespace latihribbon
             this.btn_masuk.UseVisualStyleBackColor = false;
             this.btn_masuk.Click += new System.EventHandler(this.btn_masuk_Click);
             // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 44);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 23);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Nama  :";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 11);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 23);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "NIS     : ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(95, 18);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(95, 54);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Green;
@@ -125,16 +81,11 @@ namespace latihribbon
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.btn_Keluar);
             this.Controls.Add(this.btn_masuk);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "FormMilih";
             this.Text = "FormMilih";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -142,10 +93,6 @@ namespace latihribbon
 
         private System.Windows.Forms.Button btn_Keluar;
         private System.Windows.Forms.Button btn_masuk;
-        private Label label2;
-        private Label label1;
-        private Label label3;
-        private Label label4;
         private Button button1;
     }
 }
