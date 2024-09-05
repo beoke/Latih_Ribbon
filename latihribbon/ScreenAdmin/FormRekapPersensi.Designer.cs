8977526d342412f5072df463456873686e5621ea
LENOVO <LENOVO@LAPTOP-47S3D476>
2024-09-05 15:59:05 +0700

    sk sk , rng rampung

--- latihribbon/ScreenAdmin/FormRekapPersensi.Designer.cs
+++ latihribbon/ScreenAdmin/FormRekapPersensi.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRekapPersensi));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnResetFilter = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,7 +42,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.txtNama = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPrintRekap = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -50,16 +54,19 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 53);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1217, 378);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1546, 490);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnResetFilter
             // 
-            this.btnResetFilter.Location = new System.Drawing.Point(1094, 16);
+            this.btnResetFilter.Location = new System.Drawing.Point(1459, 20);
+            this.btnResetFilter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnResetFilter.Name = "btnResetFilter";
-            this.btnResetFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnResetFilter.Size = new System.Drawing.Size(100, 28);
             this.btnResetFilter.TabIndex = 39;
             this.btnResetFilter.Text = "Reset";
             this.btnResetFilter.UseVisualStyleBackColor = true;
@@ -68,116 +75,139 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(869, 20);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(1159, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.Size = new System.Drawing.Size(91, 18);
             this.label2.TabIndex = 38;
             this.label2.Text = "Keterangan :";
             // 
             // KeteranganCombo
             // 
             this.KeteranganCombo.FormattingEnabled = true;
-            this.KeteranganCombo.Location = new System.Drawing.Point(952, 18);
+            this.KeteranganCombo.Location = new System.Drawing.Point(1269, 22);
+            this.KeteranganCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.KeteranganCombo.Name = "KeteranganCombo";
-            this.KeteranganCombo.Size = new System.Drawing.Size(121, 21);
+            this.KeteranganCombo.Size = new System.Drawing.Size(160, 24);
             this.KeteranganCombo.TabIndex = 37;
             // 
             // tgldua
             // 
             this.tgldua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tgldua.Location = new System.Drawing.Point(721, 17);
-            this.tgldua.Margin = new System.Windows.Forms.Padding(2);
+            this.tgldua.Location = new System.Drawing.Point(961, 21);
+            this.tgldua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tgldua.Name = "tgldua";
-            this.tgldua.Size = new System.Drawing.Size(132, 21);
+            this.tgldua.Size = new System.Drawing.Size(175, 24);
             this.tgldua.TabIndex = 36;
             // 
             // tglsatu
             // 
             this.tglsatu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tglsatu.Location = new System.Drawing.Point(577, 17);
-            this.tglsatu.Margin = new System.Windows.Forms.Padding(2);
+            this.tglsatu.Location = new System.Drawing.Point(769, 21);
+            this.tglsatu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tglsatu.Name = "tglsatu";
-            this.tglsatu.Size = new System.Drawing.Size(132, 21);
+            this.tglsatu.Size = new System.Drawing.Size(175, 24);
             this.tglsatu.TabIndex = 35;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(521, 19);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(695, 23);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 15);
+            this.label4.Size = new System.Drawing.Size(68, 18);
             this.label4.TabIndex = 34;
             this.label4.Text = "Tanggal :";
             // 
             // txtNIS
             // 
             this.txtNIS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNIS.Location = new System.Drawing.Point(48, 17);
-            this.txtNIS.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNIS.Location = new System.Drawing.Point(64, 21);
+            this.txtNIS.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNIS.Name = "txtNIS";
-            this.txtNIS.Size = new System.Drawing.Size(84, 21);
+            this.txtNIS.Size = new System.Drawing.Size(111, 24);
             this.txtNIS.TabIndex = 33;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(11, 19);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(15, 23);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.Size = new System.Drawing.Size(40, 18);
             this.label5.TabIndex = 32;
             this.label5.Text = "NIS :";
             // 
             // txtKelas
             // 
             this.txtKelas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtKelas.Location = new System.Drawing.Point(429, 17);
-            this.txtKelas.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKelas.Location = new System.Drawing.Point(572, 21);
+            this.txtKelas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtKelas.Name = "txtKelas";
-            this.txtKelas.Size = new System.Drawing.Size(76, 21);
+            this.txtKelas.Size = new System.Drawing.Size(100, 24);
             this.txtKelas.TabIndex = 31;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(381, 19);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(508, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 15);
+            this.label3.Size = new System.Drawing.Size(53, 18);
             this.label3.TabIndex = 30;
             this.label3.Text = "Kelas :";
             // 
             // txtNama
             // 
             this.txtNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNama.Location = new System.Drawing.Point(200, 17);
-            this.txtNama.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNama.Location = new System.Drawing.Point(267, 21);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNama.Name = "txtNama";
-            this.txtNama.Size = new System.Drawing.Size(163, 21);
+            this.txtNama.Size = new System.Drawing.Size(216, 24);
             this.txtNama.TabIndex = 29;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(149, 19);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(199, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.Size = new System.Drawing.Size(56, 18);
             this.label1.TabIndex = 28;
             this.label1.Text = "Nama :";
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnPrintRekap);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(1, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1570, 588);
+            this.panel1.TabIndex = 40;
+            // 
+            // btnPrintRekap
+            // 
+            this.btnPrintRekap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrintRekap.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPrintRekap.BackgroundImage")));
+            this.btnPrintRekap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPrintRekap.FlatAppearance.BorderSize = 2;
+            this.btnPrintRekap.Location = new System.Drawing.Point(1470, 520);
+            this.btnPrintRekap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPrintRekap.Name = "btnPrintRekap";
+            this.btnPrintRekap.Size = new System.Drawing.Size(61, 46);
+            this.btnPrintRekap.TabIndex = 19;
+            this.btnPrintRekap.UseVisualStyleBackColor = true;
+            // 
             // FormRekapPersensi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1217, 431);
+            this.ClientSize = new System.Drawing.Size(1572, 654);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnResetFilter);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.KeteranganCombo);
@@ -190,10 +220,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtNama);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormRekapPersensi";
             this.Text = "FormRekapPersensi";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,5 +245,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtNama;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPrintRekap;
     }
 }
--- latihribbon/ScreenAdmin/FormRekapPersensi.cs
+++ latihribbon/ScreenAdmin/FormRekapPersensi.cs
@@ -1,13 +1,18 @@
 ﻿using latihribbon.Dal;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LicenseContext = OfficeOpenXml.LicenseContext;
 
 namespace latihribbon.ScreenAdmin
 {
@@ -20,6 +25,8 @@ namespace latihribbon.ScreenAdmin
             InitKeterangan();
             rekapPersensiDal = new RekapPersensiDal();
             dataGridView1.DataSource = rekapPersensiDal.ListData();
+
+            Event();
         }
 
         public void InitKeterangan()
@@ -27,5 +34,135 @@ namespace latihribbon.ScreenAdmin
             List<string> Keterangan = new List<string>() { "Semua", "A", "I", "S" };
             KeteranganCombo.DataSource = Keterangan;
         }
+
+
+        #region     EVENT
+        private void Event()
+        {
+            btnPrintRekap.Click += BtnPrintRekap_Click;
+        }
+
+
+        private void BtnPrintRekap_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Eksport Data ?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Print();
+            }
+
+
+        #endregion
+
+
+        public void Print()
+        {
+            // Menyetujui lisensi EPPlus
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Menampilkan dialog untuk memilih lokasi dan nama file
+            using (var saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx";
+                saveFileDialog.Title = "Save an Excel File";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string excelFilePath = saveFileDialog.FileName;
+
+                    string connectionString = "Server=(local);Database=RekapSiswa;Trusted_Connection=True;TrustServerCertificate=True";
+
+                    // Query untuk mengambil data dari database
+                    string query = @"
+                SELECT s.NIS, s.Nama, s.Persensi, s.Kelas,
+                       SUM(CASE WHEN p.Keterangan = 'A' THEN 1 ELSE 0 END) AS A,
+                       SUM(CASE WHEN p.Keterangan = 'I' THEN 1 ELSE 0 END) AS I,
+                       SUM(CASE WHEN p.Keterangan = 'S' THEN 1 ELSE 0 END) AS S
+                FROM Siswa s
+                LEFT JOIN Persensi p ON s.NIS = p.NIS
+                GROUP BY s.NIS, s.Nama, s.Persensi, s.Kelas
+                ORDER BY s.Kelas, s.NIS";
+
+                    using (var connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        using (var command = new SqlCommand(query, connection))
+                        using (var reader = command.ExecuteReader())
+                        {
+                            // Membuat file Excel baru
+                            using (var package = new ExcelPackage())
+                            {
+                                // Dictionary untuk menyimpan data per kelas
+                                var classSheets = new Dictionary<string, ExcelWorksheet>();
+
+                                int row;
+                                while (reader.Read())
+                                {
+                                    string kelas = reader["Kelas"].ToString();
+                                    ExcelWorksheet worksheet;
+
+                                    // Jika sheet untuk kelas belum ada, buat sheet baru
+                                    if (!classSheets.ContainsKey(kelas))
+                                    {
+                                        worksheet = package.Workbook.Worksheets.Add(kelas);
+                                        classSheets[kelas] = worksheet;
+
+                                        // Menambahkan header ke sheet
+                                        worksheet.Cells[1, 1].Value = "Daftar Siswa Kelas " + kelas;
+                                        worksheet.Cells[1, 1, 1, 6].Merge = true; // Menggabungkan kolom 1 sampai 6
+                                        worksheet.Cells[1, 1].Style.Font.Bold = true;
+                                        worksheet.Cells[1, 1].Style.Font.Size = 16;
+                                        worksheet.Cells[1, 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                                        worksheet.Cells[2, 1].Value = "Nis";
+                                        worksheet.Cells[2, 2].Value = "Nama Siswa";
+                                        worksheet.Cells[2, 3].Value = "Persensi";
+                                        worksheet.Cells[2, 4].Value = "A";
+                                        worksheet.Cells[2, 5].Value = "I";
+                                        worksheet.Cells[2, 6].Value = "S";
+
+                                        for (int col = 1; col <= 6; col++)
+                                        {
+                                            worksheet.Cells[2, col].Style.Font.Bold = true;
+                                            worksheet.Cells[2, col].Style.Border.BorderAround(ExcelBorderStyle.Thin);
+                                            worksheet.Cells[2, col].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                                            worksheet.Cells[2, col].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                                        }
+
+                                        // Mengatur lebar kolom
+                                        worksheet.Column(1).Width = 10; // NIS
+                                        worksheet.Column(2).Width = 30; // Nama Siswa
+                                        worksheet.Column(3).Width = 15; // Persensi
+                                        worksheet.Column(4).Width = 8; // A
+                                        worksheet.Column(5).Width = 8; // I
+                                        worksheet.Column(6).Width = 8; // S
+                                    }
+
+                                    worksheet = classSheets[kelas];
+                                    row = worksheet.Dimension?.End.Row + 1 ?? 3; // Menentukan baris untuk data baru
+
+                                    // Menulis data ke sheet
+                                    worksheet.Cells[row, 1].Value = reader["NIS"];
+                                    worksheet.Cells[row, 2].Value = reader["Nama"];
+                                    worksheet.Cells[row, 3].Value = reader["Persensi"];
+                                    worksheet.Cells[row, 4].Value = reader["A"];
+                                    worksheet.Cells[row, 5].Value = reader["I"];
+                                    worksheet.Cells[row, 6].Value = reader["S"];
+
+                                    for (int col = 1; col <= 6; col++)
+                                    {
+                                        worksheet.Cells[row, col].Style.Border.BorderAround(ExcelBorderStyle.Thin);
+                                    }
+                                }
+
+                                // Menyimpan file Excel
+                                FileInfo excelFile = new FileInfo(excelFilePath);
+                                package.SaveAs(excelFile);
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Data berhasil diekspor ke Excel dengan format khusus.");
+                }
+            }
+        }
+
     }
 }
