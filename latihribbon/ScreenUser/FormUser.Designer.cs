fa46f8dcc2cf9b42e01fa3eb10e82997e716539f
Lahh ? <lathifnurhidayat09@gmail.com>
2024-09-26 14:20:38 +0700

    Add Form Survey Kepuasan

--- latihribbon/ScreenUser/FormUser.Designer.cs
+++ latihribbon/ScreenUser/FormUser.Designer.cs
@@ -29,89 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainUser));
-            this.MainPanel = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.ButtonTerlambat = new System.Windows.Forms.Button();
-            this.ButtonIzin = new System.Windows.Forms.Button();
             this.ribbon_terlambat = new System.Windows.Forms.RibbonButton();
             this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.PictureBintang_1 = new System.Windows.Forms.PictureBox();
+            this.PictureBintang_2 = new System.Windows.Forms.PictureBox();
+            this.PictureBintang_5 = new System.Windows.Forms.PictureBox();
+            this.PictureBintang_4 = new System.Windows.Forms.PictureBox();
+            this.PictureBintang_3 = new System.Windows.Forms.PictureBox();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // MainPanel
-            // 
-            this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.MainPanel.Location = new System.Drawing.Point(285, 77);
-            this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(866, 607);
-            this.MainPanel.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.BackColor = System.Drawing.Color.Gray;
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1151, 81);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.panel3.BackColor = System.Drawing.Color.Silver;
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.ButtonIzin);
-            this.panel3.Controls.Add(this.ButtonTerlambat);
-            this.panel3.Location = new System.Drawing.Point(0, 77);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(288, 607);
-            this.panel3.TabIndex = 2;
-            // 
-            // ButtonTerlambat
-            // 
-            this.ButtonTerlambat.BackColor = System.Drawing.Color.Transparent;
-            this.ButtonTerlambat.FlatAppearance.BorderSize = 0;
-            this.ButtonTerlambat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ButtonTerlambat.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonTerlambat.ForeColor = System.Drawing.Color.Black;
-            this.ButtonTerlambat.Image = ((System.Drawing.Image)(resources.GetObject("ButtonTerlambat.Image")));
-            this.ButtonTerlambat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonTerlambat.Location = new System.Drawing.Point(-1, 3);
-            this.ButtonTerlambat.Name = "ButtonTerlambat";
-            this.ButtonTerlambat.Size = new System.Drawing.Size(284, 64);
-            this.ButtonTerlambat.TabIndex = 3;
-            this.ButtonTerlambat.Text = "          Data Siswa Terlambat";
-            this.ButtonTerlambat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonTerlambat.UseVisualStyleBackColor = false;
-            this.ButtonTerlambat.UseWaitCursor = true;
-            // 
-            // ButtonIzin
-            // 
-            this.ButtonIzin.BackColor = System.Drawing.Color.Transparent;
-            this.ButtonIzin.FlatAppearance.BorderSize = 0;
-            this.ButtonIzin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ButtonIzin.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonIzin.ForeColor = System.Drawing.Color.Black;
-            this.ButtonIzin.Image = ((System.Drawing.Image)(resources.GetObject("ButtonIzin.Image")));
-            this.ButtonIzin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonIzin.Location = new System.Drawing.Point(-1, 73);
-            this.ButtonIzin.Name = "ButtonIzin";
-            this.ButtonIzin.Size = new System.Drawing.Size(284, 64);
-            this.ButtonIzin.TabIndex = 4;
-            this.ButtonIzin.Text = "          Data Siswa Izin";
-            this.ButtonIzin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ButtonIzin.UseVisualStyleBackColor = false;
-            this.ButtonIzin.UseWaitCursor = true;
             // 
             // ribbon_terlambat
             // 
@@ -129,44 +61,88 @@
             this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
             this.ribbonButton1.Text = "Terlambat";
             // 
-            // pictureBox1
+            // MainPanel
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(129, 79);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPanel.BackColor = System.Drawing.Color.LightGray;
+            this.MainPanel.Controls.Add(this.PictureBintang_3);
+            this.MainPanel.Controls.Add(this.PictureBintang_4);
+            this.MainPanel.Controls.Add(this.PictureBintang_5);
+            this.MainPanel.Controls.Add(this.PictureBintang_2);
+            this.MainPanel.Controls.Add(this.PictureBintang_1);
+            this.MainPanel.Location = new System.Drawing.Point(32, 27);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1090, 631);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // PictureBintang_1
+            // 
+            this.PictureBintang_1.Location = new System.Drawing.Point(135, 419);
+            this.PictureBintang_1.Name = "PictureBintang_1";
+            this.PictureBintang_1.Size = new System.Drawing.Size(100, 100);
+            this.PictureBintang_1.TabIndex = 0;
+            this.PictureBintang_1.TabStop = false;
+            // 
+            // PictureBintang_2
+            // 
+            this.PictureBintang_2.Location = new System.Drawing.Point(309, 419);
+            this.PictureBintang_2.Name = "PictureBintang_2";
+            this.PictureBintang_2.Size = new System.Drawing.Size(100, 100);
+            this.PictureBintang_2.TabIndex = 1;
+            this.PictureBintang_2.TabStop = false;
+            // 
+            // PictureBintang_5
+            // 
+            this.PictureBintang_5.Location = new System.Drawing.Point(831, 419);
+            this.PictureBintang_5.Name = "PictureBintang_5";
+            this.PictureBintang_5.Size = new System.Drawing.Size(100, 100);
+            this.PictureBintang_5.TabIndex = 2;
+            this.PictureBintang_5.TabStop = false;
+            // 
+            // PictureBintang_4
+            // 
+            this.PictureBintang_4.Location = new System.Drawing.Point(657, 419);
+            this.PictureBintang_4.Name = "PictureBintang_4";
+            this.PictureBintang_4.Size = new System.Drawing.Size(100, 100);
+            this.PictureBintang_4.TabIndex = 3;
+            this.PictureBintang_4.TabStop = false;
+            // 
+            // PictureBintang_3
+            // 
+            this.PictureBintang_3.Location = new System.Drawing.Point(483, 419);
+            this.PictureBintang_3.Name = "PictureBintang_3";
+            this.PictureBintang_3.Size = new System.Drawing.Size(100, 100);
+            this.PictureBintang_3.TabIndex = 4;
+            this.PictureBintang_3.TabStop = false;
             // 
             // FormMainUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1151, 683);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.MainPanel);
-            this.Controls.Add(this.panel3);
             this.Name = "FormMainUser";
             this.Text = "FormMainUser";
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.MainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBintang_3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel MainPanel;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button ButtonTerlambat;
-        private System.Windows.Forms.Button ButtonIzin;
         private System.Windows.Forms.RibbonButton ribbon_terlambat;
         private System.Windows.Forms.RibbonButton ribbonButton1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox PictureBintang_3;
+        private System.Windows.Forms.PictureBox PictureBintang_4;
+        private System.Windows.Forms.PictureBox PictureBintang_5;
+        private System.Windows.Forms.PictureBox PictureBintang_2;
+        private System.Windows.Forms.PictureBox PictureBintang_1;
     }
 }
