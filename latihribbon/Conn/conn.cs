f8a5c2913fc4e5e126670a9941b1252577e83f55
Rifki Yoga <rys9612345@gmail.com>
2024-10-08 14:08:18 +0700

    ssd

--- latihribbon/Conn/conn.cs
+++ latihribbon/Conn/conn.cs
@@ -12,21 +12,7 @@ namespace latihribbon.Conn
     {
         public static string connstr()
         {
-            //string conn = "Server = (local);Database = RekapSiswa;Trusted_Connection = True;TrustServerCertificate = True";
-            string conn = "Server = 192.168.195.107; Database = RekapSiswa; User Id = RESI ; Password = ATM_RekapSiswa";
-            try
-            {
-                using (var connection = new SqlConnection(conn))
-                {
-                    connection.Open();
-                    return conn;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Koneksi ke database terputus: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return string.Empty;
-            }
+            return "Server = (local);Database = RekapSiswa;Trusted_Connection = True;TrustServerCertificate = True";
         }
     }
 }
