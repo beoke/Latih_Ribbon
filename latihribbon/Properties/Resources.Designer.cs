d48e383d96bba3693e92995cc4d87e13905b0c2f
Rifki Yoga <rys9612345@gmail.com>
2024-11-09 22:25:41 +0700

    w

--- latihribbon/Properties/Resources.Designer.cs
+++ latihribbon/Properties/Resources.Designer.cs
@@ -213,36 +213,6 @@ namespace latihribbon.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap left_arrow {
-            get {
-                object obj = ResourceManager.GetObject("left-arrow", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap left_arrow_removebg_preview {
-            get {
-                object obj = ResourceManager.GetObject("left-arrow-removebg-preview", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap LeftArrowAbu {
-            get {
-                object obj = ResourceManager.GetObject("LeftArrowAbu", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap LeftArrowAbu1 {
             get {
                 object obj = ResourceManager.GetObject("LeftArrowAbu1", resourceCulture);
