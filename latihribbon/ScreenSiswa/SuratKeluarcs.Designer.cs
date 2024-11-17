253c1b2e9616191c1cc9ff7527fd877ac2d54e9e
Lathif Nur Hidayat <lathifnurhidayat09@gmail.com>
2024-11-17 13:43:26 +0700

    .

--- latihribbon/ScreenSiswa/SuratKeluarcs.Designer.cs
+++ latihribbon/ScreenSiswa/SuratKeluarcs.Designer.cs
@@ -132,7 +132,7 @@
             this.ButtonCaraPengisianJam.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ButtonCaraPengisianJam.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonCaraPengisianJam.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ButtonCaraPengisianJam.Location = new System.Drawing.Point(746, 455);
+            this.ButtonCaraPengisianJam.Location = new System.Drawing.Point(746, 432);
             this.ButtonCaraPengisianJam.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonCaraPengisianJam.Name = "ButtonCaraPengisianJam";
             this.ButtonCaraPengisianJam.Size = new System.Drawing.Size(43, 37);
@@ -158,7 +158,7 @@
             this.label13.AutoSize = true;
             this.label13.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label13.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(317, 119);
+            this.label13.Location = new System.Drawing.Point(317, 116);
             this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(23, 34);
@@ -170,7 +170,7 @@
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label12.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(317, 203);
+            this.label12.Location = new System.Drawing.Point(317, 195);
             this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(23, 34);
@@ -182,7 +182,7 @@
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label10.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(317, 288);
+            this.label10.Location = new System.Drawing.Point(317, 274);
             this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(23, 34);
@@ -194,7 +194,7 @@
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(317, 372);
+            this.label8.Location = new System.Drawing.Point(317, 353);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(23, 34);
@@ -206,7 +206,7 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(317, 455);
+            this.label7.Location = new System.Drawing.Point(317, 432);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(23, 34);
@@ -217,7 +217,7 @@
             // 
             this.txtTanggal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtTanggal.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTanggal.Location = new System.Drawing.Point(370, 288);
+            this.txtTanggal.Location = new System.Drawing.Point(370, 274);
             this.txtTanggal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtTanggal.Name = "txtTanggal";
             this.txtTanggal.ReadOnly = true;
@@ -231,7 +231,7 @@
             this.jamKembali.CustomFormat = "HH:mm";
             this.jamKembali.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.jamKembali.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.jamKembali.Location = new System.Drawing.Point(370, 455);
+            this.jamKembali.Location = new System.Drawing.Point(370, 432);
             this.jamKembali.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.jamKembali.Name = "jamKembali";
             this.jamKembali.ShowUpDown = true;
@@ -267,7 +267,7 @@
             this.tx_keluar.BackColor = System.Drawing.SystemColors.Control;
             this.tx_keluar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tx_keluar.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tx_keluar.Location = new System.Drawing.Point(370, 372);
+            this.tx_keluar.Location = new System.Drawing.Point(370, 353);
             this.tx_keluar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tx_keluar.Name = "tx_keluar";
             this.tx_keluar.ReadOnly = true;
@@ -280,7 +280,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(44, 203);
+            this.label2.Location = new System.Drawing.Point(44, 197);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 34);
@@ -292,7 +292,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(44, 288);
+            this.label3.Location = new System.Drawing.Point(44, 277);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(168, 34);
@@ -303,7 +303,7 @@
             // 
             this.txtKelas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtKelas.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtKelas.Location = new System.Drawing.Point(370, 203);
+            this.txtKelas.Location = new System.Drawing.Point(370, 195);
             this.txtKelas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtKelas.Name = "txtKelas";
             this.txtKelas.ReadOnly = true;
@@ -316,7 +316,7 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(44, 455);
+            this.label6.Location = new System.Drawing.Point(44, 437);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(237, 34);
@@ -328,7 +328,7 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(42, 119);
+            this.label4.Location = new System.Drawing.Point(42, 117);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(86, 34);
@@ -340,7 +340,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(44, 372);
+            this.label5.Location = new System.Drawing.Point(44, 357);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(217, 34);
@@ -351,7 +351,7 @@
             // 
             this.txtNama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtNama.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNama.Location = new System.Drawing.Point(370, 119);
+            this.txtNama.Location = new System.Drawing.Point(370, 116);
             this.txtNama.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNama.Name = "txtNama";
             this.txtNama.ReadOnly = true;
